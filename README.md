266d065803b5d68e4d0f63181dcd684dabf095e9
astrogewgaw <ujjwalpanda97@gmail.com>
2021-05-21 17:08:17 +0530

    :recycle: Refactoring how scraping works.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Code style: black][black-badge]][black]
 
-this is my own copy of the database of pulsar glitches maintained [**here**][table] by the Jodrell Bank Centre for Astrophysics at the University of Manchester. if you end up using the data from the original table or from this repository, please cite [**Espinoza et al. (2011)**][paper] (a BibTeX style citation for the same is given in the [**CITATION.md**](CITATION.md) file) and refer to the original [**url**][glitches]. the data, in all of its JSON glory, is available in the [**glitches.json**](glitches.json) file. the whole scraping code resides in the [**scrap.py**](scrap.py) file. the data is updated on the 11th of every month, at midnight. this repository will eventually power the [**koshka**][koshka] package, which aims to make accessing all pulsar and radio transient related catalogues easier.
+this is my own copy of the database of pulsar glitches maintained [**here**][table] by the Jodrell Bank Centre for Astrophysics at the University of Manchester. if you end up using the data from the original table or from this repository, please cite [**Espinoza et al. (2011)**][paper] (a BibTeX style citation for the same is given in the [**CITATION.md**](CITATION.md) file) and refer to the original [**url**][glitches]. the data, in all of its JSON glory, is available in the [**glitches.json**](glitches.json) file. the whole scraping code resides in the [**scrap.py**](scrap.py) file. the data is updated on every Friday, at midnight (in UTC time). this repository will eventually power the [**koshka**][koshka] package, which aims to make accessing all pulsar and radio transient related catalogues easier.
 
 [black]: https://github.com/psf/black
 [koshka]: https://github.com/astrogewgaw/koshka
--- scrap.py
+++ scrap.py
@@ -1,81 +1,58 @@
-import json
-import requests
+from json import dump
+from requests import get
+from bs4 import BeautifulSoup
+from typing import Dict, Union, Optional
 
-from typing import Dict
-from functools import partial
-from bs4 import Tag, BeautifulSoup
-from schema import Or, Use, Schema
-
-# url:  URL of the glitches database.
-# H:    The header length.
-# F:    The footer length.
-url = "http://www.jb.man.ac.uk/pulsar/glitches/gTable.html"
-H = 5
-F = 16
-
-
-def references(tag: Tag):
-
-    """
-    Parse the references as (name, link) pairs into a dictionary.
-    """
-
-    return {
-        "REFS": {
-            str(i + 1): atag["href"]
-            for (
-                i,
-                atag,
-            ) in enumerate(tag("a"))
-        }
-    }
+data: Dict[
+    str,
+    Dict[
+        str,
+        Optional[
+            Union[
+                str,
+                int,
+                Dict,
+                float,
+            ]
+        ],
+    ],
+] = {}
 
 
-# A simple function to deal with numeric values that could possibly be NULL.
-isnull = lambda x, A: A * float(x) if x != "X" else None
-
-# The key-value map for the database.
-kvs = {
-    "NAME": Use(str),
-    "JNAME": Use(str),
-    "COUNT": Use(int),
-    "MJD": Use(partial(isnull, A=1.0)),
-    "MJD_ERR": Use(partial(isnull, A=1.0)),
-    "DF/F": Use(partial(isnull, A=1e-9)),
-    "DF/F_ERR": Use(partial(isnull, A=1e-9)),
-    "DF1/F1": Use(partial(isnull, A=1e-3)),
-    "DF1/F1_ERR": Use(partial(isnull, A=1e-3)),
-    "REFS": Or({}, {str: str}),
-}
+rows = BeautifulSoup(
+    markup=get("http://www.jb.man.ac.uk/pulsar/glitches/gTable.html").content,
+    features="lxml",
+).table("tr")[5:-16]
 
 
-def scrap() -> None:
-
-    """
-    Scrap the glitches database and serialise it into a JSON file.
-    """
-
-    glitches: Dict = {}
-
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "lxml")
-    rows = soup.table("tr")[H:-F]
-
-    for i, row in enumerate(rows):
-        refs = row("td")[-1]
-        values = [item.text.strip() for item in row("td")[1:-1]]
-        glitch = {key: value for key, value in zip(kvs.keys(), values)}
-        glitch.update(references(refs))
-        glitches[str(i + 1)] = Schema(kvs).validate(glitch)
-
-    with open("glitches.json", "w+") as fobj:
-        json.dump(
-            glitches,
-            fobj,
-            indent=4,
+for i, row in enumerate(rows):
+    cells = [_.text.strip() for _ in row("td")[1:-1]]
+    glitch = {
+        key: (None if cell == "X" else factor * conv(cell))
+        for (key, conv, factor), cell in zip(
+            [
+                ("NAME", str, 1),
+                ("JNAME", str, 1),
+                ("Glitch number", int, 1),
+                ("MJD", float, 1),
+                ("MJD_ERR", float, 1),
+                ("DeltaF/F", float, 1e-9),
+                ("DeltaF/F_ERR", float, 1e-9),
+                ("DeltaF1/F1", float, 1e-3),
+                ("DeltaF1/F1_ERR", float, 1e-3),
+            ],
+            cells,
         )
+    }
+    glitch["References"] = {
+        str(k + 1): _["href"] for k, _ in enumerate(row("td")[-1]("a"))
+    }
+    data[str(i + 1)] = glitch
 
 
-if __name__ == "__main__":
-
-    scrap()
+with open("glitches.json", "w+") as fobj:
+    dump(
+        obj=dict(data=data),
+        fp=fobj,
+        indent=4,
+    )
